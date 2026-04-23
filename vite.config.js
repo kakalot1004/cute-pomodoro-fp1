7e18b798998491c5512c53a5cafccca6677bb062
kkuli <275414740+kakalot1004@users.noreply.github.com>
2026-04-23 09:29:31 +0900

    Fix: Change base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cute-pomodoro/', // 깃허브 저장소 이름에 맞춰 기본 경로 설정!
+  base: '/', // Vercel 배포를 위해 루트 경로로 수정!
 })
